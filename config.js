e7f783a51ed072a9877688f75601142b9067f2c5
Dennis1337x <113421270+Dennis1337x@users.noreply.github.com>
2024-03-31 09:49:00 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    sniperGuild: "1217468163567128586",
+    sniperToken: "MTIwNTc0OTMwODAzMzYwMTU3Ng.Go9r_C.nbEpA4W3FAmGmh3OjvZJCHEGPD9exkK9re95PA",
+    listenerToken: "MTIwNTc0OTMwODAzMzYwMTU3Ng.Go9r_C.nbEpA4W3FAmGmh3OjvZJCHEGPD9exkK9re95PA", 
+    webhooks: "https://discord.com/api/webhooks/1212077399454523443/zyPT-TVfgrPJzuUXQZx0UcTl-jDbWQJw3x29PhMnxbTAlDAq3YhDiwUfBB0-SpdccNtL",
+  };
+  
+		
